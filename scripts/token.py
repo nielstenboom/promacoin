ab39392458da4e11e389518d9b784da6e12eb355
Niels ten Boom <nielstenboom@gmail.com>
2021-09-30 19:23:13 +0200

    using ropsten contract address

--- scripts/token.py
+++ scripts/token.py
@@ -4,5 +4,5 @@ from brownie import Token, accounts
 
 
 def main():
-    acct = accounts.load('ganache')
+    acct = accounts.load('local')
     return Token.deploy("Test Token", "PROMACOIN", 18, 1e21, {'from': acct})
